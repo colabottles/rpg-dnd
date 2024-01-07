62370dd317963e050f58f9280ae814ba04f44d28
Todd Libby <toddlibby@protonmail.com>
2024-01-06 19:53:22 -0700

    Minor change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Adventure Async Awaits',
-  tagline: 'Crits &amp; Giggles',
+  tagline: 'Crits & Giggles',
   url: 'https://crits-n-giggles.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
